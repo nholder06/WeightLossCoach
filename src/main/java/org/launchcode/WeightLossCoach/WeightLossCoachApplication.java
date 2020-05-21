94ae716ec6f5def15d55d477d73e7a39ada04582
Nicole Holder <nholder06@gmail.com>
2020-05-21 17:26:02 -0500

    Added index thymeleaf template and index controller

---
+++ src/main/java/org/launchcode/WeightLossCoach/WeightLossCoachApplication.java
@@ -0,0 +1,14 @@
+package org.launchcode.WeightLossCoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightLossCoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeightLossCoachApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/org/launchcode/WeightLossCoach/WeightLossCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.WeightLossCoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightLossCoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
