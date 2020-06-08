b462c15b5714ebea3551b72948458ddefd40d74b
Nicole Holder <nholder06@gmail.com>
2020-06-08 17:15:06 -0500

    added testimonial object

---
+++ src/main/java/org/launchcode/WeightLossCoach/models/TestimonialClient.java
@@ -0,0 +1,10 @@
+package org.launchcode.WeightLossCoach.models;
+
+import org.springframework.data.mongodb.core.mapping.Field;
+
+public class TestimonialClient extends AbstractEntity{
+
+    @Field
+    private String name;
+
+}
