ac05e7aef6bf6cc62e8952ffb1a3597d21344f34
Nicole Holder <nholder06@gmail.com>
2020-06-08 18:10:12 -0500

    updating lost work

--- src/main/java/org/launchcode/WeightLossCoach/controllers/FormController.java
+++ src/main/java/org/launchcode/WeightLossCoach/controllers/FormController.java
@@ -9,12 +9,10 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
 @Controller
-@RequestMapping("home")
 public class FormController {
 
     @Autowired
--- src/main/java/org/launchcode/WeightLossCoach/controllers/TestimonialsController.java
+++ src/main/java/org/launchcode/WeightLossCoach/controllers/TestimonialsController.java
@@ -1,4 +1,11 @@
 package org.launchcode.WeightLossCoach.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestimonialsController {
+
+    @GetMapping("/testimonials")
+    public String testimonialView() {return "home/testimonials";}
 }
